7e498522e8b510be97c1aa93e8355e67ec2af15a
ElenaAleksandrovna <elenasidorova9219@gmail.com>
2022-02-21 10:24:49 +0300

    init: add sorting function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let resArray = [];
+  if (!Array.isArray(matrix) || matrix.length == 0){
+    return [];
+  }
+  for (let i = 0; i < matrix.length; i++){
+    resArray.push(...(i % 2 === 1) ? matrix[i].reverse() : matrix[i]);
+  }
+  return resArray;
 }
